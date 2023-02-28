6114dbbc6da94a6215daf145f971c1fd1e621507
Constantine <Constantine2903@gmail.com>
2023-02-28 23:05:46 +0200

    finished refactoring

--- bot/handler.py
+++ bot/handler.py
@@ -3,6 +3,7 @@ import re
 
 from commands import AddressBook, Record, Name, Phone, Birthday
 
+
 class Handler:
     """
     Bot processor
@@ -21,8 +22,8 @@ class Handler:
 
     def run(self, request: tuple[str, dict]):
         return self.main_commands[request.create_command()[0]](request)
-    
-    def hello(self, request: None): # No need info for this command
+
+    def hello(self, request: None):  # No need info for this command
         responces = ['Hi!',
                      'How can I help you?',
                      'How are you?',
@@ -33,29 +34,30 @@ class Handler:
         command = request.create_command()[1]
         self.address_book.add_record(Record(**command))
         print('Line has been added')
-        
+
     def change(self, request: dict):
         command = request.create_command()[1]
         record = Record(name=command.pop('name'))
         print(record.change_field(**command))
-        
+
     def search(self, request: dict):
         command = request.create_command()[1]
         print("Here what I've found")
         print(self.address_book.search(**command))
-        
+
     def get_birthday(self, request: dict):
         command = request.create_command()[1]
         print(Record(name=command['name']).days_to_birthday())
-        
+
     def show(self, request: dict):
         command = request.create_command()[1]
         print("Here what I've got")
         for i in self.address_book.iterator(command):
             print(i)
-        
-    def show_all(self, request: None): # No need info for this command
+
+    def show_all(self, request: None):  # No need info for this command
         print(self.address_book)
+
 
 class Request:
     """
@@ -64,9 +66,9 @@ class Request:
     """
     def __init__(self, request: str):
         self.request = request
-        self.__request = None
+        self.__request = None  # Do not touch. __get_command doesn't work without that
         self.main_commands = Handler().main_commands
-        
+
     def create_command(self) -> tuple[str, dict]:
         """
         Collecting info for the command
@@ -75,15 +77,15 @@ class Request:
             tuple[str, dict]: (command name, command info dict)
         """
         command = self.__get_command()
-        
+
         if command == 'add':
             name = self.__get_name()
             birthday = self.__get_birthday()
             phones = self.__get_phone(self.__request)
-            return (command, {'name': name, 
-                              'birthday': birthday, 
+            return (command, {'name': name,
+                              'birthday': birthday,
                               'phones': phones})
-            
+
         elif command == 'change':
             name = self.__get_name()
             separator = self.__request.find('to')
@@ -94,22 +96,22 @@ class Request:
             return (command, {'name': name,
                               'old_field': old_field,
                               'new_field': new_field})
-            
+
         elif command == 'search':
             search = self.__request
             return (command, {'search': search})
-            
+
         elif command == 'birthday':
             name = self.__get_name()
             return (command, {'name': name})
-        
+
         elif command in ('hello', 'show all'):
             return (command, )
-        
+
         elif command == 'show':
             page_length = re.search(r'\d+', self.__request).group()
             return (command, int(page_length))
-            
+
     def __get_command(self):
         main_commands = self.main_commands.keys()
         for i in main_commands:
@@ -117,23 +119,23 @@ class Request:
             if command:
                 self.__request = re.sub(fr'{command.group()}', '', self.request).strip()
                 return command.group().lower()
-    
+
     def __get_name(self):
         name = re.search(r"\b([A-Z][a-z]+)+", self.__request)
         return Name(name.group())
-    
+
     def __get_phone(self, request: str):
         phones = re.findall(r'\b\+?[\(\)\-\d]+\b', request)
         if phones:
             return [Phone(i) for i in phones if Phone(i).value]
-        
+
     def __get_birthday(self):
         birthday = re.search(r'\d{,2}[/.-]\d{,2}[/.-]\d{,4}', self.__request)
         if birthday:
             return Birthday(birthday.group())
 
 
-if __name__ == '__main__':    
+if __name__ == '__main__':
     test_req = Request('ADD Bill  +3809894989, 6548941351, 135-2568-465 (23432)3423434 birthday 25/02/2023')
     test_req2 = Request('change Bill 3809894989, 6548941351 to 0951115544')
     test_req3 = Request('get birthday Bill')
